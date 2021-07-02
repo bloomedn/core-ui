75f7fbd44626a986526b36d8ac20f245f8384fbf
Ram K Rijal <rijal.ramk@gmail.com>
2021-07-03 00:09:57 +0200

    links

--- Link/index.js
+++ Link/index.js
@@ -1,5 +1,8 @@
 import NextLink from "next/link";
+import { Link } from 'components/link';
+import arrowAngle from 'assets/arrow-angle.svg';
 
-export default function Link({ name, href }) {
-    return <NextLink href={href}>{name}</NextLink>;
-}
+// export default function Link({ name, href }) {
+//     return <NextLink href={href}>{name}</NextLink>;
+// }
+
