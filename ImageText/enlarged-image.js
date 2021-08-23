3dbe05a6f6ebe4f16810cfe7336068e3e76ed77c
Ram K Rijal <rijal.ramk@gmail.com>
2021-08-23 12:36:24 +0200

    images

--- ImageText/enlarged-image.js
+++ ImageText/enlarged-image.js
@@ -2,23 +2,26 @@
 import MobileDesktopImage from "remote/core-ui/MobileDesktopImage";
 import SanityImage from "components/SanityImage";
 
-export function EnlargedImage({ image, textOnRight }) {
+export function EnlargedImage({ image }) {
     return (
         <div
             sx={{
                 // my: [0, null, null, null, -5],
                 position: "relative",
-                top: [-5],
+                top: [5],
+                px : [10],
+                justifyContent : 'center',
                 borderRadius: ["2rem"],
+                height : '100%',
                 // borderBottom: "4px solid",
                 borderColor: "primary",
                 overflow: "hidden",
                 "&:hover": {
                     border: "4px solid",
                     borderColor: "primary",
-                    transform: textOnRight
-                        ? "rotate(-2.5deg)"
-                        : "rotate(2.5deg)",
+                    // transform: textOnRight
+                    //     ? "rotate(-2.5deg)"
+                    //     : "rotate(2.5deg)",
                 },
             }}
         >
