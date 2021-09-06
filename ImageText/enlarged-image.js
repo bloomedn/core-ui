048ec91821e8c3432376705614e2713aa99329ea
Ram K Rijal <rijal.ramk@gmail.com>
2021-09-07 00:06:57 +0200

    ImageText

--- ImageText/enlarged-image.js
+++ ImageText/enlarged-image.js
@@ -3,42 +3,47 @@ import MobileDesktopImage from "remote/core-ui/MobileDesktopImage";
 import SanityImage from "components/SanityImage";
 
 export function EnlargedImage({ image }) {
-    return (
-        <div
-            sx={{
-                // my: [0, null, null, null, -5],
-                position: "relative",
-                top: [5],
-                px : [10],
-                justifyContent : 'center',
-                borderRadius: ["2rem"],
-                height : '100%',
-                // borderBottom: "4px solid",
-                borderColor: "primary",
-                overflow: "hidden",
-                "&:hover": {
-                    border: "4px solid",
-                    borderColor: "primary",
-                    // transform: textOnRight
-                    //     ? "rotate(-2.5deg)"
-                    //     : "rotate(2.5deg)",
-                },
-            }}
-        >
-            <MobileDesktopImage
-                desktop={<SanityImage photo={image?.defaultImage} />}
-                mobile={
-                    <SanityImage
-                        photo={
-                            image?.mobileImage?.asset
-                                ? image?.mobileImage
-                                : image?.defaultImage
-                        }
-                    />
-                }
-            />
-        </div>
-    );
+  return (
+    <div
+      sx={{
+        // my: [0, null, null, null, -5],
+        position: "relative",
+        top: [5],
+        // px: [10],
+        justifyContent: "center",
+        borderRadius: ["2rem"],
+        // height: "100%",
+        position: "relative",
+        // display: "flex",
+        // zIndex: 99,
+        // flexGrow: 2,
+        // borderRadius: "50%",
+        // borderBottom: "4px solid",
+        borderColor: "primary",
+        overflow: "hidden",
+        "&:hover": {
+          border: "4px solid",
+          borderColor: "primary",
+          // transform: textOnRight
+          //     ? "rotate(-2.5deg)"
+          //     : "rotate(2.5deg)",
+        },
+      }}
+    >
+      <MobileDesktopImage
+        desktop={<SanityImage photo={image?.defaultImage} />}
+        mobile={
+          <SanityImage
+            photo={
+              image?.mobileImage?.asset
+                ? image?.mobileImage
+                : image?.defaultImage
+            }
+          />
+        }
+      />
+    </div>
+  );
 }
 
 const styles = {};
