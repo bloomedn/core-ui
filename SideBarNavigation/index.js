033940caeb18c082d76d56d892c41eff9402b216
Ram K Rijal <rijal.ramk@gmail.com>
2021-09-07 13:39:08 +0200

    sidebar

--- SideBarNavigation/index.js
+++ SideBarNavigation/index.js
@@ -2,31 +2,31 @@
 import { Box, Grid } from "theme-ui";
 import Link from "next/link";
 
-export default function SideBarNavigation({ navigation, child }) {
-    return (
-        <Grid sx={styles.grid}>
-            <div sx={styles.navigator}>{navigation}</div>
-            <div sx={styles.line}></div>
-            <div sx={styles.child}>{child}</div>
-        </Grid>
-    );
+export default function SideBarNavigation({ navigation, child, pageTitle }) {
+  return (
+    <Grid sx={styles.grid}>
+      <div sx={styles.navigator}>{navigation}</div>
+      <div sx={styles.line}></div>
+      <div sx={styles.child}>{child}</div>
+    </Grid>
+  );
 }
 
 export function SiderBarNavigationItem({ title, href }) {
-    return (
-        <Link href={href} scroll={false}>
-            <Box sx = {styles.linkTitle}> {title} </Box>
-        </Link>
-    );
+  return (
+    <Link href={href} scroll={false}>
+      <Box sx={styles.linkTitle}> {title} </Box>
+    </Link>
+  );
 }
 
 const styles = {
-    grid: { gridTemplateColumns: ["25% 5px 2fr"], gap: [6] },
-    line: { width: "5px", height: "100%", bg: "lightestPrimary" },
-    child: {},
+  grid: { gridTemplateColumns: ["25% 5px 2fr"], gap: [6] },
+  line: { width: "5px", height: "100%", bg: "lightestPrimary" },
+  child: {},
 
-    linkTitle : {
-        cursor: "pointer",
-        justifyContent: 'center',
-    }
+  linkTitle: {
+    cursor: "pointer",
+    justifyContent: "center",
+  },
 };
