ec9033f8c0561abb916ad645a6c2ede2098e73a1
Ram K Rijal <rijal.ramk@gmail.com>
2021-09-15 23:58:09 +0200

    index

--- FAQ/index.js
+++ FAQ/index.js
@@ -1,14 +1,14 @@
 /** @jsxImportSource theme-ui */
-import Accordion from 'components/accordion/accordion';
+import Accordion from "components/Accordion/accordion";
 
 export const Faq = ({ question, answer }) => {
-    return (
-        <div>
-            {/* <p>{question}</p>
+  return (
+    <div>
+      {/* <p>{question}</p>
             {open && <p>{answer}</p>} */}
-            {/* <Accordion items={[{title : question, contents : answer }]} /> */}
-        </div>
-    );
+      {/* <Accordion items={[{title : question, contents : answer }]} /> */}
+    </div>
+  );
 };
 
 export default Faq;
